73a167fd231c2f0bf78341e8769d974f41ceeacc
gene lazarin <159508631+genelazarin86@users.noreply.github.com>
2024-02-09 18:52:23 -0600

    Add files via upload

---
+++ ACTIVIDAD 2/eva 1 actividad 2/src/main/java/com/mycompany/evaluacion1/Evaluacion1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evaluacion1;
+
+/**
+ *
+ * @author leo
+ */
+public class Evaluacion1 {
+
+    public static void main(String[] args) {
+        int edad;
+        String nombre;
+        double salario;
+        edad = 50;
+        nombre = "gene ricardo lazarin";
+        salario = 123.3;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+   
+    
+    
+        
+       
+     
+        
+    }
+
