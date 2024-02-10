c7bdda5bf5e5dd109403a96f2600fc4a272341a2
gene lazarin <159508631+genelazarin86@users.noreply.github.com>
2024-02-09 18:55:59 -0600

    Add files via upload

---
+++ actividad 1 eva 1/EVA1actividad1/src/main/java/com/mycompany/eva1actividad1/EVA1actividad1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1actividad1;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA1actividad1 {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo!");
+    }
+}
