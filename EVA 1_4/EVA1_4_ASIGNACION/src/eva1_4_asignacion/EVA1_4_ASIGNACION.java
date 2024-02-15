698790b662773862daf1e9be6df3251ceca11364
gene lazarin <159508631+genelazarin86@users.noreply.github.com>
2024-02-15 16:55:10 -0600

    Add files via upload

---
+++ EVA 1_4/EVA1_4_ASIGNACION/src/eva1_4_asignacion/EVA1_4_ASIGNACION.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int edad;
+        edad = 20;
+        System.out.println("tu edad es");
+        System.out.println(edad);
+        String nombre;
+        nombre = "ricardo";
+        System.out.println(nombre);
+                String carrera;
+                carrera = "sistemas computacionales";
+                System.out.println(carrera);
+    }
+    
+}
